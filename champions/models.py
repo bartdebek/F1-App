e1170b37844c0dc41e23ec1a46de7ff076baeefb
debus3 <debus3@gmail.com>
2022-09-04 23:05:45 +0200

    change rating icons,add teams pages

--- champions/models.py
+++ champions/models.py
@@ -2,7 +2,6 @@ from datetime import date, timedelta
 from django.db import models
 
 
-
 class Country(models.Model):
     name = models.CharField(max_length=50)
     flag = models.ImageField(upload_to='images/flags/')
@@ -29,6 +28,9 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
+    def get_flag(self):
+        return self.country.flag
+
     
 class Driver(models.Model):
     first_name = models.CharField(max_length=20)
@@ -52,7 +54,7 @@ class Driver(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-
+    
     def get_flag(self):
         return self.nationality.flag
 
--- champions/urls.py
+++ champions/urls.py
@@ -5,13 +5,20 @@ from .views import (
     DriversDetailView,
     DriversListCurrentView,
     DriversListPastView,
+    TeamsListView,
+    TeamsDetailView,
 )
 
 
 urlpatterns = [
-    path('', DriversListView.as_view(), name='home'),
-    path('current/', DriversListCurrentView.as_view(), name='driver_current' ),
-    path('past/', DriversListPastView.as_view(), name='driver_past' ),
+    path('driver/', DriversListView.as_view(), name='home'),
+    path('driver/current/', DriversListCurrentView.as_view(), name='driver_current' ),
+    path('driver/past/', DriversListPastView.as_view(), name='driver_past' ),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('<int:pk>/', DriversDetailView.as_view(), name='driver_detail'),
+    path('driver/<int:pk>/', DriversDetailView.as_view(), name='driver_detail'),
+
+    path('team/', TeamsListView.as_view(), name='team_list'),
+    path('team/current/', DriversListCurrentView.as_view(), name='team_current' ),
+    path('team/past/', DriversListPastView.as_view(), name='team_past' ),
+    path('team/<int:pk>/', TeamsDetailView.as_view(), name='team_detail'),
 ]
